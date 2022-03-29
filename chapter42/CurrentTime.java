3065426188d0f505b98dced99e35937bb5c63ece
Mavaddat Javid <javid@mavaddat.ca>
2022-03-29 12:08:49 -0700

    Add files via upload

---
+++ chapter42/CurrentTime.java
@@ -0,0 +1,16 @@
+package chapter42;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class CurrentTime extends HttpServlet {
+  /** Process the HTTP Get request */
+  public void doGet(HttpServletRequest request, HttpServletResponse
+      response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<p>The current time is " + new java.util.Date());
+    out.close(); // Close stream
+  }
+}
---
+++ chapter42/MixedContent.java
@@ -0,0 +1,24 @@
+package chapter42; 
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MixedContent extends HttpServlet {
+  /** Process the HTTP Get request */
+  public void doGet(HttpServletRequest request, HttpServletResponse
+      response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+    String country = request.getParameter("country");
+
+    out.println("<img src = \"" +
+      "ImageContentWithDrawing\" align=right>");
+
+    out.println("This is a clock created using a Java program " +
+      "and sent to the browser by a servlet.");
+    
+    out.close();
+  }
+}
